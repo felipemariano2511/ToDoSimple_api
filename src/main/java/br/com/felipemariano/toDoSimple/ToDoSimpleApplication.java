f2b1ba32c3e3a4975ff1717e24fa6b98d60d8a9b
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-22 12:08:52 -0300

    Configuração de conexão com o banco PostgreSQL

--- src/main/java/br/com/felipemariano/toDoSimple/ToDoSimpleApplication.java
+++ src/main/java/br/com/felipemariano/toDoSimple/ToDoSimpleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoSimpleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ToDoSimpleApplication.class, args);
 	}
 
