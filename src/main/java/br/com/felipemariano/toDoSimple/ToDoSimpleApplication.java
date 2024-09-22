d88dc62ea319bc6f508533017f5cc7003f9f050f
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-22 11:29:42 -0300

    Projeto de API em Spring Boot

---
+++ src/main/java/br/com/felipemariano/toDoSimple/ToDoSimpleApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipemariano.toDoSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoSimpleApplication.class, args);
+	}
+
+}
