d090a28a24f5731744f07433c1c1d7d8413ee6e2
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-23 09:24:58 -0300

    Criação dos Repositorys de User e Task

---
+++ src/main/java/br/com/felipemariano/toDoSimple/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package br.com.felipemariano.toDoSimple.repositories;
+
+import br.com.felipemariano.toDoSimple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+}
