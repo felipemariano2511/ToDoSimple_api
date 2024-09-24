f92d93ce429136a8e0fc9c4187f5b22d1101ff42
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-24 11:29:22 -0300

    Adição do método findAllUserId

---
+++ src/main/java/br/com/felipemariano/toDoSimple/controllers/TaskController.java
@@ -0,0 +1,2 @@
+package br.com.felipemariano.toDoSimple.controllers;public class TaskController {
+}
--- src/main/java/br/com/felipemariano/toDoSimple/services/TaskService.java
+++ src/main/java/br/com/felipemariano/toDoSimple/services/TaskService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +22,12 @@ public class TaskService {
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()));
+    }
+
+    public List<Task> findAllUserId (Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+
+        return tasks;
     }
 
     @Transactional
