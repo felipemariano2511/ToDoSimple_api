cdade6ab00378140b447ff9c5411972503087c87
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-24 18:02:31 -0300

    Criação do WebConfig

--- src/main/java/br/com/felipemariano/toDoSimple/configs/WebConfig.java
+++ src/main/java/br/com/felipemariano/toDoSimple/configs/WebConfig.java
@@ -1,2 +1,16 @@
-package br.com.felipemariano.toDoSimple.configs;public class WebConfig {
+package br.com.felipemariano.toDoSimple.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
 }
