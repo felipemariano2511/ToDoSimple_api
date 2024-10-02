247d2e61ed994678013cb0eff5494728d118903a
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-10-02 11:02:09 -0300

    Criação do UserSpringSecurity

---
+++ src/main/java/br/com/felipemariano/toDoSimple/models/enums/ProfileEnum.java
@@ -0,0 +1,2 @@
+package br.com.felipemariano.toDoSimple.models.enums;public class ProfileEnum {
+}
---
+++ src/main/java/br/com/felipemariano/toDoSimple/security/UserSpringSecurity.java
@@ -0,0 +1,54 @@
+package br.com.felipemariano.toDoSimple.security;
+
+import br.com.felipemariano.toDoSimple.models.enums.ProfileEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String passwrod;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String username, String passwrod, Set<ProfileEnum> profileEnums) {
+        this.id = id;
+        this.username = username;
+        this.passwrod = passwrod;
+        this.authorities = profileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+}
